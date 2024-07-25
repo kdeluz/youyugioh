87ed1f8326f72108f841dad704bb7788bf581da9
kdeluz <kdeluz@rrc.ca>
2024-07-25 14:11:26 -0500

    completed; added new changes via provinces beinga dropdown menu for more consistency and lessens room for error

---
+++ app/helpers/provinces_helper.rb
@@ -0,0 +1,20 @@
+module ProvincesHelper
+    def province_options
+      [
+        ["Alberta", "Alberta"],
+        ["British Columbia", "British Columbia"],
+        ["Manitoba", "Manitoba"],
+        ["New Brunswick", "New Brunswick"],
+        ["Newfoundland and Labrador", "Newfoundland and Labrador"],
+        ["Nova Scotia", "Nova Scotia"],
+        ["Ontario", "Ontario"],
+        ["Prince Edward Island", "Prince Edward Island"],
+        ["Quebec", "Quebec"],
+        ["Saskatchewan", "Saskatchewan"],
+        ["Northwest Territories", "Northwest Territories"],
+        ["Nunavut", "Nunavut"],
+        ["Yukon", "Yukon"]
+      ]
+    end
+  end
+  
