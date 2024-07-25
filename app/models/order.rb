dd30350b86d15541535f204881c01c0c429e60c3
kdeluz <kdeluz@rrc.ca>
2024-07-25 13:49:39 -0500

    checkout process is completed, implenenting tax is now starting

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,12 @@ class Order < ApplicationRecord
   accepts_nested_attributes_for :order_items
 
   validates :address_line1, :city, :province, :postal_code, :country, presence: true
+
+  def calculate_order_total
+    total = 0
+    self.order_items.each do |item|
+      total += item.product.price * item.quantity
+    end
+    total
+  end
 end
