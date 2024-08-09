d1c96972cc528ed59b720ccf7e14911bdc6afe35
kdeluz <kdeluz@rrc.ca>
2024-08-08 21:04:25 -0500

    reverted changes on orders.rb

--- app/admin/invoices.rb
+++ app/admin/invoices.rb
@@ -1,49 +1,62 @@
+
 ActiveAdmin.register Invoice do
-    permit_params :user_id, :order_id, :invoice_number, :total, :gst, :pst, :hst, :qst
-  
-    index do
-      selectable_column
-      id_column
-      column :invoice_number
-      column :total
-      column :gst
-      column :pst
-      column :hst
-      column :qst
-      column :created_at
-      actions
+  permit_params :user_id, :order_id, :invoice_number, :total, :gst, :pst, :hst, :qst, :invoice_state
+
+  index do
+    selectable_column
+    id_column
+    column :invoice_number
+    column :total
+    column :gst
+    column :pst
+    column :hst
+    column :qst
+    column :invoice_state do |invoice|
+      status_tag invoice.invoice_state, label: invoice.invoice_state.humanize
     end
-  
-    show do
-      attributes_table do
-        row :invoice_number
-        row :total
-        row :gst
-        row :pst
-        row :hst
-        row :qst
-        row :created_at
-        row :updated_at
+    column :created_at
+    actions do |invoice|
+      item "Mark as Shipped", mark_as_shipped_admin_invoice_path(invoice), method: :put if invoice.paid?
+    end
+  end
+
+  show do
+    attributes_table do
+      row :invoice_number
+      row :total
+      row :gst
+      row :pst
+      row :hst
+      row :qst
+      row :invoice_state do |invoice|
+        status_tag invoice.invoice_state, label: invoice.invoice_state.humanize
       end
-  
-      panel "Order Details" do
-        attributes_table_for invoice.order do
-          row :id
-          row :address_line1
-          row :city
-          row :province
-          row :postal_code
-          row :country
-        end
+      row :created_at
+      row :updated_at
+    end
+
+    panel "Order Details" do
+      attributes_table_for invoice.order do
+        row :id
+        row :address_line1
+        row :city
+        row :province
+        row :postal_code
+        row :country
       end
-  
-      panel "Order Items" do
-        table_for invoice.order.order_items do
-          column :product
-          column :quantity
-          column :price
-        end
+    end
+
+    panel "Order Items" do
+      table_for invoice.order.order_items do
+        column :product
+        column :quantity
+        column :price
       end
     end
   end
-  
+
+  member_action :mark_as_shipped, method: :put do
+    resource.mark_as_shipped
+    redirect_to admin_invoice_path(resource), notice: "Invoice marked as shipped!"
+  end
+end
